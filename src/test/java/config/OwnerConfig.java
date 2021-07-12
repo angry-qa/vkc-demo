495ba8a28bb43a4b1ffb56034f249f23c7c195b1
Dmitry Trofimov <dm.trofimov@corp.vk.com>
2021-07-12 18:18:08 +0300

    selenoid fix

--- src/test/java/config/OwnerConfig.java
+++ src/test/java/config/OwnerConfig.java
@@ -21,7 +21,7 @@ public interface OwnerConfig extends Config {
     @Key("web.maximized")
     boolean isMaximized();
 
-    @DefaultValue("false")
+    @DefaultValue("true")
     @Key("webdriver.remote")
     boolean isRemote();
 
